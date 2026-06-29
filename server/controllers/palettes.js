9d5c155e75e88dbac074b12fa428090af198a56e
Wanyi <ng.wanyi.000@gmail.com>
2026-06-28 21:36:33 -0400

    update controller functions to account for palette_colors table

--- server/controllers/palettes.js
+++ server/controllers/palettes.js
@@ -2,16 +2,28 @@ import pool from '../config/database.js'
 
 const createPalette = async (req, res) => {
   try {
-    const { name, theme, mood, favorite, description, tags, created_at, updated_at } = req.body
+    const { name, theme, mood, favorite, description, tags, colors } = req.body
 
     const { rows } = await pool.query(
-      `INSERT INTO gifts (name, theme, mood, favorite, description, tags, created_at, updated_at)
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8)
-        RETURNING *`,
-      [name, theme, mood, favorite, description, tags, created_at, updated_at]
+      `INSERT INTO palettes (name, theme, mood, favorite, description, tags)
+        VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING *
+      ;`,
+      [name, theme, mood, favorite, description, tags]
     )
 
-    res.status(201).json(rows[0])
+    const palette = rows[0]
+
+    for (let i = 0; i < colors.length; i++) {
+      await pool.query(
+        `INSERT INTO palette_colors (palette_id, hex, position, is_locked)
+          VALUES ($1, $2, $3, $4)
+        ;`,
+        [palette.id, colors[i].hex, i + 1, colors[i].is_locked]
+      )
+    }
+
+    res.status(201).json({ ...palette, colors })
   } catch (error) {
     res.status(409).json({ error: error.message })
   }
@@ -20,9 +32,21 @@ const createPalette = async (req, res) => {
 const getPalettes = async (req, res) => {
   try {
     const { rows } = await pool.query(
-      `SELECT * FROM palettes ORDER BY id ASC`
+      `SELECT
+        p.*,
+        COALESCE(
+          (
+            SELECT json_agg(pc ORDER BY pc.position)
+            FROM palette_colors pc
+            WHERE pc.palette_id = p.id
+          ),
+          '[]'
+        ) AS colors
+        FROM palettes p
+        ORDER BY p.id ASC
+      ;`
     )
-    res.status(200).json(rows[0])
+    res.status(200).json(rows)
   } catch (error) {
     res.status(409).json({ error: error.message })
   }
@@ -32,7 +56,19 @@ const getPaletteById = async (req, res) => {
   try {
     const id = parseInt(req.params.id)
     const { rows } = await pool.query(
-      `SELECT palettes.* FROM palettes WHERE id = $1`,
+      `SELECT
+          p.*,
+          COALESCE(
+              (
+                SELECT json_agg(pc ORDER BY pc.position)
+                FROM palette_colors pc
+                WHERE pc.palette_id = p.id
+              ),
+              '[]'
+          ) AS colors
+        FROM palettes p
+        WHERE p.id = $1
+      ;`,
       [id]
     )
     res.status(200).json(rows[0])
@@ -44,11 +80,28 @@ const getPaletteById = async (req, res) => {
 const updatePalette = async (req, res) => {
   try {
     const id = parseInt(req.params.id)
-    const { name, theme, mood, favorite, description, tags, created_at, updated_at } = req.body
+    const { name, theme, mood, favorite, description, tags, colors } = req.body
     const { rows } = await pool.query(
-      `UPDATE palettes SET name = $1, theme = $2, mood = $3, favorite = $4, description = $5, tags = $6, created_at = $7, updated_at = $8, WHERE id = $9`,
-      [name, theme, mood, favorite, description, tags, created_at, updated_at, id]  
+      `UPDATE palettes
+        SET name = $1, theme = $2, mood = $3, favorite = $4, description = $5, tags = $6, updated_at = CURRENT_TIMESTAMP
+        WHERE id = $7
+        RETURNING *
+      ;`,
+      [name, theme, mood, favorite, description, tags, id]  
     )
+
+    await pool.query(`DELETE FROM palette_colors WHERE palette_id = $1`, [id])
+
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i]
+      const position = i + 1
+      await pool.query(
+        `INSERT INTO palette_colors (palette_id, hex, position, is_locked)
+        VALUES ($1, $2, $3, $4)`,
+        [id, color.hex, position, color.is_locked]
+      )
+    }
+
     res.status(200).json(rows[0])
   } catch (error) {
     res.status(409).json({ error: error.message })
@@ -59,7 +112,10 @@ const deletePalette = async (req, res) => {
   try {
     const id = parseInt(req.params.id)
     const { rows } = await pool.query(
-      `DELETE FROM palettes WHERE id = $1`,
+      `DELETE FROM palettes
+        WHERE id = $1
+        RETURNING *
+      ;`,
       [id]
     )
     res.status(200).json(rows[0])
